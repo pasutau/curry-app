30106286b6b74a0b243b524729d58aff04cdbd48
pasutau <30033474+pasutau@users.noreply.github.com>
2021-03-10 08:07:36 +0900

    パスワード変更画面更新

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,13 +5,13 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">{{ __('Reset Password') }}</div>
+        <div class="card-header">パスワード再設定</div>
         <div class="card-body">
           <form method="POST" action="{{ route('password.update') }}">
             @csrf
             <input type="hidden" name="token" value="{{ $token }}">
             <div class="form-group row">
-              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+              <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
               <div class="col-md-6">
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
                   value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
@@ -23,7 +23,7 @@
               </div>
             </div>
             <div class="form-group row">
-              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+              <label for="password" class="col-md-4 col-form-label text-md-right">変更後パスワード</label>
               <div class="col-md-6">
                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
                   name="password" required autocomplete="new-password">
@@ -36,16 +36,16 @@
             </div>
             <div class="form-group row">
               <label for="password-confirm"
-                class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                class="col-md-4 col-form-label text-md-right">変更後パスワード（再入力）</label>
               <div class="col-md-6">
                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required
                   autocomplete="new-password">
               </div>
             </div>
-            <div class="form-group row mb-0">
+            <div class="row mb-0">
               <div class="col-md-6 offset-md-4">
                 <button type="submit" class="btn btn-primary">
-                  {{ __('Reset Password') }}
+                  パスワード変更
                 </button>
               </div>
             </div>
